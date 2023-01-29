78c0d764a501453852ad0f1742ef6327e75cd00f
NPlanche <89564236+NPlanche@users.noreply.github.com>
2023-01-28 20:25:08 -0500

    update

---
+++ Api/Api/Api/Controllers/ApiController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Proxy.Controllers
+{
+    /// <summary>
+    /// HomeController
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        IConfiguration _configuration;
+
+        public ApiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return StatusCode(200, "Listening...");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Upload
+        /// reference
+        /// https://learn.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-7.0
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                if (file.Length > 0)
+                {
+                    var path = _configuration["Folder"].ToString();
+
+                    bool exists = Directory.Exists(path);
+
+                    if (!exists)
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    var filePath = $"{path}{file.FileName}";
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// GetFiles
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetFiles")]
+        public IActionResult GetFiles()
+        {
+            try
+            {
+                string[] files = GetFileNames(_configuration["Folder"].ToString(), "*.*");
+
+                return StatusCode(200, files);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// GetFile
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetFile/{fileName}")]
+        public IActionResult GetFile(string fileName)
+        {
+            try
+            {
+                string[] files = GetFileNames(_configuration["Folder"].ToString(), "*.*");
+
+                var file = files.SingleOrDefault(p => p == fileName);
+
+                return StatusCode(200, file);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// GetFileNames
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        private static string[] GetFileNames(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+            }
+
+            return files;
+        }
+    }
+}
---
+++ Proxy/Proxy/Proxy/Controllers/ProxyController.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Proxy.Controllers
+{
+    /// <summary>
+    /// HomeController
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class ProxyController : ControllerBase
+    {
+        IConfiguration _configuration;
+
+        public ProxyController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return StatusCode(200, "Listening...");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Upload
+        /// reference
+        /// https://stackoverflow.com/questions/39397278/post-files-from-asp-net-core-web-api-to-another-asp-net-core-web-api
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("UploadFile")]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            try
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string endPointAPI = _configuration["EndPointAPI"].ToString();
+
+                    using (var client = new HttpClient())
+                    {
+                        try
+                        {
+                            client.BaseAddress = new Uri(endPointAPI);
+
+                            byte[] data;
+
+                            using (var br = new BinaryReader(file.OpenReadStream()))
+                            {
+                                data = br.ReadBytes((int)file.OpenReadStream().Length);
+                            }
+
+                            ByteArrayContent bytes = new ByteArrayContent(data);
+
+                            MultipartFormDataContent multiContent = new MultipartFormDataContent();
+
+                            multiContent.Add(bytes, "file", file.FileName.Replace(".jpeg",".myjpeg"));
+
+                            var result = client.PostAsync("Upload", multiContent).Result;
+                            
+                            return StatusCode((int)result.StatusCode);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            return StatusCode(500, ex.Message);
+                        }
+                    }
+                }
+
+                return StatusCode(400, "Bad Request");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Files
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetFiles")]
+        public async Task<IActionResult> GetFiles()
+        {
+            try
+            {
+                string endPointAPI = _configuration["EndPointAPI"].ToString();
+
+                using (var client = new HttpClient())
+                {
+                    try
+                    {
+                        client.BaseAddress = new Uri(endPointAPI);
+
+                        var responseTask = await client.GetAsync("GetFiles");
+
+                        if (responseTask.IsSuccessStatusCode)
+                        {
+                            var apiResp = await responseTask.Content.ReadAsStringAsync();
+
+                            return StatusCode((int)responseTask.StatusCode, apiResp);
+                        }
+                        else
+                        {
+                            return StatusCode((int)responseTask.StatusCode, "something when wrong");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return StatusCode(500, ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Files
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetFile/{fileName}")]
+        public IActionResult GetFile(string fileName)
+        {
+            try
+            {
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
